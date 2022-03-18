7b500a155c5221547ab058d9084a9e0d449ccc55
Lesleycg28 <lesley28calderon@gmail.com>
2022-03-17 23:14:15 -0500

    Clase Planta_electrica

---
+++ src/Planta_electrica.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class Planta_electrica extends Ciudad {
+	private Region region;
+	private Ciudad ciudad;
+	private ArrayList<Integer> consumo;
+	private double tarifa;
+	
+	public Planta_electrica(String nombre) {
+		super(nombre);
+	}
+
+	public ArrayList<Integer> getConsumo() {
+		return consumo;
+	}
+
+	public void setConsumo(ArrayList<Integer> consumo) {
+		this.consumo = consumo;
+	}
+	
+	public double getTarifa() {
+		return tarifa;
+	}
+	
+	public void setTarifa(double tarifa) {
+		this.tarifa = tarifa;
+	}
+
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	public ArrayList<Integer> consumoCiudades() {
+		consumo = new ArrayList<Integer>();
+		int i = 0;
+		while(i < 12) {
+			consumo.add((int) (Math.random()*(500+40)));
+			i++;
+		}
+		return consumo;
+	}
+	
+	public double tarifas(double tarifas) {
+		int contador = 0;
+		for(int i = 0; i < consumo.size(); i++) {
+			contador++;
+		}
+		return tarifa = (double) tarifas*contador;
+	}
+	
+	@Override
+	public String toString() {
+		return " Consumos: "+consumo;
+	}
+
+}
+
